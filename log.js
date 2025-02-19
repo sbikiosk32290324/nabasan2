83f3493e8c4d2dd157b8bfa50c812ad84551c9e8
sbikiosk <164787041+sbikiosk32290324@users.noreply.github.com>
2025-02-19 23:05:16 +0530

    Update log.js

--- log.js
+++ log.js
@@ -188,3 +188,20 @@ function GetPrint()
     /*For Print*/
     window.print();
 }
+function SetCurrentDate()
+{
+    const date = new Date();
+    console.log(date);
+
+    let d = date.getDate();
+    let m = date.getMonth() + 1;
+    let y = date.getFullYear();
+
+    if (d < 10) d = '0' + d;
+    if (m < 10) m = '0' + m;
+
+    let CurrDate = y + '-' + m + '-' + d;
+
+    $('input[name="inv_dt"]').val(CurrDate);
+
+}
